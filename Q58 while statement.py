98742bd4abbb6a75f0294679a41ac9123fd09d93
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:48:22 +0530

    Update Q58 while statement.py

--- Q58 while statement.py
+++ Q58 while statement.py
@@ -3,4 +3,4 @@ count = 1
 while count <= 5:
     print("Count is:", count)
     count += 1
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
