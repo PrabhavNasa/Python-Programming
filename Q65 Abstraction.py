25652228386ac8426a1c20dd29cbda200176db2b
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:46:59 +0530

    Update Q65 Abstraction.py

--- Q65 Abstraction.py
+++ Q65 Abstraction.py
@@ -14,4 +14,4 @@ cat = Cat()
 print(dog.speak())
 print(cat.speak())
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
