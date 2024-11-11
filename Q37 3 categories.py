73e978b5da7172953e098155fb304b757d5a72dc
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:52:44 +0530

    Update Q37 3 categories.py

--- Q37 3 categories.py
+++ Q37 3 categories.py
@@ -17,4 +17,4 @@ print(f"Even numbers: {evens}")
 print(f"\nOdd numbers: {odds}")
 print(f"\nNumbers divisible by 5: {divisible_by_5}")
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
