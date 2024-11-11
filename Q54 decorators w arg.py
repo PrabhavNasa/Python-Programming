039bec470de626db220b05240ca9948901e7cfcd
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:48:58 +0530

    Update Q54 decorators w arg.py

--- Q54 decorators w arg.py
+++ Q54 decorators w arg.py
@@ -14,4 +14,4 @@ def greet(name):
     print(f"Hello, {name}!")
 greet("Tanisha")
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
