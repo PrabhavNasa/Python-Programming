6025b0b9d9329ad4a40d69d55dbed7d6692eceec
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:53:37 +0530

    Update Q32 armstrong 2.py

--- Q32 armstrong 2.py
+++ Q32 armstrong 2.py
@@ -12,4 +12,4 @@ for i in range(low_limit,upp_limit +1):
         counter = counter + 1
         print(f"conter: {counter}")
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
