a8b314909b859c54614aa9c2f3fcc31dbfd23026
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:47:23 +0530

    Update Q63 Inheritance.py

--- Q63 Inheritance.py
+++ Q63 Inheritance.py
@@ -19,4 +19,4 @@ class Car(Vehicle):
 my_car = Car("Toyota", "Corolla", 2020, "Petrol")
 my_car.display_info()
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
