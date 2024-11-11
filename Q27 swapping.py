43576858ba3aff72d885275fa245bad76cfc2dfe
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:54:36 +0530

    Update Q27 swapping.py

--- Q27 swapping.py
+++ Q27 swapping.py
@@ -16,4 +16,4 @@ b=a-b
 a=a-b
 print(f'Numbers after swapping: {a}, {b}')
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
