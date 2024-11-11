5098cb9a888af8356063b2524141ac1e6581f54c
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:52:07 +0530

    Update Q40 palindrome.py

--- Q40 palindrome.py
+++ Q40 palindrome.py
@@ -5,4 +5,4 @@ def main():
     else:
         print(f'User entered string is not a palindrome')
 main()
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
