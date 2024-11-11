e62f3fd7f70608c47363ca0d19df85be38c4cd85
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:43:01 +0530

    Add files via upload

---
+++ Q46 Comprehensions.py
@@ -0,0 +1,15 @@
+print('List Comprehensions:'.upper())
+squares = [x**2 for x in range(1,6)] 
+even_squares = [x**2 for x in range(1,6) if x % 2 == 0] 
+print("Squares:", squares)
+print("Even squares:", even_squares)
+
+print('\nList Comprehension with Condition:'.upper())
+even = [x for x in range(1,11) if x % 2 == 0]
+print('Even Numbers:', even)
+
+print('\nSet Comprehension with Condition:'.upper())
+squares = {x**2 for x in range(1,6)}
+print('Squares:',squares)
+
+print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
---
+++ Q50 Set Operations.py
@@ -0,0 +1,16 @@
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+union = set1 | set2
+intersection = set1 & set2
+diff1 = set1 - set2
+diff2 = set2 - set1
+sym_diff = set1 ^ set2
+print(set1)
+print(set2)
+print("Union:", union)
+print("Intersection:", intersection)
+print("Difference (Only Set1):", diff1)
+print("Difference (Only Set2):", diff2)
+print("Symmetric Difference:", sym_diff)
+
+print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
