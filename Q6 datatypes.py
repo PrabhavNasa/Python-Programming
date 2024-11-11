c6a7921f0ab0beb8dcb176d0908503193ffbfa69
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:48:03 +0530

    Update Q6 datatypes.py

--- Q6 datatypes.py
+++ Q6 datatypes.py
@@ -30,4 +30,4 @@ print("Dictionary:", dictionary)
 none = None
 print("None:", none)
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
