408f5ed579bb2eb2f697bc20c08358a868767d79
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:50:47 +0530

    Update Q47 Lambda Expressions with map().py

--- Q47 Lambda Expressions with map().py
+++ Q47 Lambda Expressions with map().py
@@ -1,4 +1,4 @@
 numbers = [1, 2, 3, 4]
 squared = list(map(lambda x: x**2, numbers))
 print(squared)
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
