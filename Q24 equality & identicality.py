829d0d5fce8cfe8cd0b0854f17a7d5b10c7af528
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:55:04 +0530

    Update Q24 equality & identicality.py

--- Q24 equality & identicality.py
+++ Q24 equality & identicality.py
@@ -3,4 +3,4 @@ a=b=[1,2,3]
 c=[1,2,3]
 print('id(a):',id(a),'id(b):',id(b),'id(c):',id(c))
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
