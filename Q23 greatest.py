f004ba9346a63b93340743838b4d04c0dabae919
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:55:16 +0530

    Update Q23 greatest.py

--- Q23 greatest.py
+++ Q23 greatest.py
@@ -13,4 +13,4 @@ elif c>a and c>b:
 else:
     print("All numbers are equal.")
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
