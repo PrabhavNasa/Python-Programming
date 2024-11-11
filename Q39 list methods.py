55dbec5a5700890148fbb9735548d920c88f8258
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:52:26 +0530

    Update Q39 list methods.py

--- Q39 list methods.py
+++ Q39 list methods.py
@@ -15,4 +15,4 @@ print("First element:", l[0])
 print("Last element:", l[-1])
 print("Sliced list:", l[1:len(l)-1])
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
