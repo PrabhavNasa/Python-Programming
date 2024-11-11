5ab64bd2af197ffabbf392e9acd8f089b4c39f1e
PrabhavNasa <142142259+PrabhavNasa@users.noreply.github.com>
2024-11-11 10:47:54 +0530

    Update Q60 Module.py

--- Q60 Module.py
+++ Q60 Module.py
@@ -24,4 +24,4 @@ import math_utils as mu
 result = mu.add(5, 3) 
 print(result)
 
-print("\nThis program is written by Tanisha. \nERPID: 0221BCA066")
+print("\nThis program is written by Prabhav. \nERPID: 0221BCA011")
